1badf753125c10061313b32d7c3ebc83557468df
wesleyalves <110191958+Wesley1980alves@users.noreply.github.com>
2022-09-07 22:04:26 -0300

    Create treinando e apredendo 2.c
    estudos sobre logica de programaçao

---
+++ treinando e apredendo 2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+                    /*treinado e aprendendo*/
+int main(){
+        int valor1,valor2,valor3,valor4,valor5,valor6,valor7;
+        printf("digite um valor qualquer inteiro\n");
+        scanf("%d",&valor1);
+        printf("digite um segundo valor qualquer inteiro\n");
+        scanf("%d",&valor2);
+        printf("digite um terceiro valor qualquer \n");
+        scanf("%d",&valor3);
+        printf("digite um quarto valor qualquer\n");
+        scanf("%d",&valor4);
+        printf("digite um quinto valor qualquer\n");
+        scanf("%d",&valor5);
+        printf("digite um sexto valor qualquer \n");
+        scanf("%d",&valor6);
+        printf("digite um setimo valor qualquer\n");
+        scanf("%d",&valor7);
+        printf("primeiro = %d  segundo = %d  terceiro = %d quarto = %d  quinto = %d  sexto = %d  setimo %d\n",valor1, valor2,
+                 valor3,valor4,valor5,valor6,valor7);
+
+
+
+    return 0;
+}
